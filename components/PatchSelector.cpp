0440c656f8f7858481f329cddb5f8a2162e3ea1d
Johannes Unger <unger@digitalklang.de>
2024-04-14 02:49:24 +0200

    total patch

--- components/PatchSelector.cpp
+++ components/PatchSelector.cpp
@@ -1,15 +1,33 @@
 #include "PatchSelector.h"
 #include <iostream>
 #include <integra7/presets/SnAcousticPresets.h>
+#include <cassert>
+
+namespace
+{
+    constexpr size_t TotalNumPresets = i7::NumSnAcousticPresets;
+    const i7::Preset* Presets[TotalNumPresets];
+    bool _createTotalPresets()
+    {
+        size_t total = 0;
+        for (size_t i = 0; i < i7::NumSnAcousticPresets; ++i)
+        {
+            assert(i < TotalNumPresets);
+            Presets[total++] = &i7::SnAcousticPresets[i];
+        }
+        return true;
+    }
+    bool _ = _createTotalPresets();
+}
 
 PatchSelector::PatchSelector() : SearchableCombobox()
 {
     setDataSource(
-        []() { return i7::NumSnAcousticPresets; }, 
-        [](size_t index) { return juce::String(i7::SnAcousticPresets[index].fullName); },
+        []() { return TotalNumPresets; },
+        [](size_t index) { return juce::String(Presets[index]->fullName); },
         [](size_t index, const juce::String &str)
         {
-            juce::String name(i7::SnAcousticPresets[index].fullName);
+            juce::String name(Presets[index]->fullName);
             return name.containsIgnoreCase(str);
         }
     );
