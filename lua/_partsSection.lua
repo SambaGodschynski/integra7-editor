d1a20f43c728a913dc360cec631ffab1f8dfdcd3
samba <samba@manjaro>
2025-08-26 01:38:02 +0200

    impl part type

--- lua/_partsSection.lua
+++ lua/_partsSection.lua
@@ -3,6 +3,32 @@ require "_sysex"
 
 local p = ParameterSetValueWrapper;
 local get = GetWrapper
+local toneTypes = {"SN-A", "SN-S", "SN-D", "PCM-S", "PCM-D"}
+local toneTypeValues = {
+    { msb=89, lsb=64 },
+    { msb=95, lsb=64 },
+    { msb=88, lsb=64 },
+    { msb=87, lsb=64 },
+    { msb=86, lsb=64 },
+}
+
+local function partTypeChange(part)
+    return function (value)
+        local values = toneTypeValues[value]
+        local msbId = "PRM-_PRF-_FP" .. tostring(part) .. "-NEFP_PAT_BS_MSB"
+        local msbMessage = CreateSysexMessage(msbId, values.msb)
+
+        local lsbId = "PRM-_PRF-_FP" .. tostring(part) .. "-NEFP_PAT_BS_LSB"
+        local lsbMessage = CreateSysexMessage(lsbId, values.lsb)
+
+        local pcId = "PRM-_PRF-_FP" .. tostring(part) .. "-NEFP_PAT_PC"
+        local pcMessage = CreateSysexMessage(pcId, 0)
+
+        local msg = Concat(msbMessage, lsbMessage)
+        msg = Concat(msg, pcMessage)
+        return msg
+    end
+end
 
 function CreatePartsSections(main)
     local parts = {
@@ -17,10 +43,13 @@ function CreatePartsSections(main)
             params = {}
         }
         table.insert(subSection.params,
-            p({type="range", id="PRM-_PRF-_FP".. i .."-NEFP_LEVEL", name=get(partName ..  " Level"), min=get(0), max=get(127), default=100})
+            p({type="range", id="PRM-_PRF-_FP"..i.."-NEFP_LEVEL", name=get(partName ..  " Level"), min=get(0), max=get(127), default=100})
         )
         table.insert(subSection.params,
             p({type="toggle", id="PRM-_PRF-_FP"..i.."-NEFP_MUTE_SW", name=get(partName ..  " Mute"), min=get(0), max=get(1)})
+        )
+        table.insert(subSection.params,
+            {type="select", id="PRM-_PRF-_FP"..i.."-NEFP_TYPE_DUMMY", name=get(partName.." Type"), default=1, options=toneTypes, setValue=partTypeChange(i)}
         )
         table.insert(parts.grp, subSection)
     end
