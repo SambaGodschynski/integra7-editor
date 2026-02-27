f84cd199747653d08d9e91807603b957c3c5646a
samba <samba@manjaro>
2026-02-27 23:59:41 +0100

    replacing sn-d note tabs with selection

--- lua/_sndTabsSection.lua
+++ lua/_sndTabsSection.lua
@@ -1,29 +1,7 @@
-require "math"
-
-local NoteNames = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"}
-
-local function noteName(note)
-    local octave = math.floor(note / 12) - 1
-    local name = NoteNames[(note % 12) + 1]
-    return name .. tostring(octave)
-end
-
 function CreateSndTabSections(main)
     for partNr = 1, 16, 1 do
         local pn     = string.format("%02d", partNr)
         local prefix = "Part " .. pn .. " SN-D "
-
-        -- Drum Inst tabs (notes 27-88)
-        local instTabs = {}
-        for note = 27, 88, 1 do
-            local nn   = noteName(note)
-            local chKey = prefix .. "Inst " .. nn
-            table.insert(instTabs, {label = nn, keys = {chKey}})
-            local s = main[chKey]
-            if s then s.hideFromPalette = true end
-        end
-        local k = prefix .. "Inst"
-        main[k] = {name = k, tabs = instTabs}
 
         -- CompEq tabs (6 channels)
         local compEqTabs = {}
@@ -33,7 +11,7 @@ function CreateSndTabSections(main)
             local s = main[chKey]
             if s then s.hideFromPalette = true end
         end
-        k = prefix .. "CompEq"
+        local k = prefix .. "CompEq"
         main[k] = {name = k, tabs = compEqTabs}
     end
 end
